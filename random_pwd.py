fb53dcc891b10fd6c19dbdd99e6954bdd6811cee
nderitu-ndegwa <nderitundegwa10@gmail.com>
2021-06-25 13:02:41 +0300

    1st project commit

---
+++ random_pwd.py
@@ -0,0 +1,19 @@
+#import the necessary pythn librraries
+import random, string
+
+#ask user to input password length and assign it to pwd_length
+pwd_length = int(input("Please input password length in figures: "))
+
+#Define characters to be included in the password
+pwd_characters = string.digits + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
+
+#Assign password to blank initially
+pwd = []
+
+#Define the logic
+for i in range(pwd_length):
+    pwd.append(random.choice(pwd_characters))
+
+#Display the password to the user using the print function
+password = ''.join(str(pwd))
+print(password)
